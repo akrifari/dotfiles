7fc26f856c6c220155f4be378bb2d00e1c8cdd89
Krisan Alifari <krisan.alifari@gmail.com>
2019-11-11 04:56:45 +0700

    Add script to connect external monitor

---
+++ .local/bin/external-monitor.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/sh
+
+monitor=$(xrandr | grep "eDP1" | cut -d " " -f 1)
+ext_monitor=$(xrandr | awk "/ connected/ && ! /$monitor/" | cut -d " " -f 1 | head -n 1)
+
+if [ "$ext_monitor" =  "" ]; then
+  dunstify -u low "There is no external monitor connected"
+  exit 1
+fi
+
+if [ "$ext_monitor" = "DP1" ]; then
+  size="800x600"
+else
+  size="1280x720"
+fi
+
+case $1 in
+  -s)
+    xrandr --output $monitor --mode $size --output $ext_monitor --same-as $monitor --mode $size
+    dunstify "External monitor same as primary"
+    ;;
+  -h)
+    xrandr --output $ext_monitor --mode $size --left-of $monitor
+    dunstify "External monitor left of primary"
+    ;;
+  -l)
+    xrandr --output $ext_monitor --mode $size --right-of $monitor
+    dunstify "External monitor right of primary"
+    ;;
+  -r)
+    xrandr --output $ext_monitor --off --output $monitor --mode $size
+    dunstify "Only display primary monitor"
+    ;;
+esac
