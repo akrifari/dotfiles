12923fa44ac8d8f90e96c9f42e918bef1ec8a07c
Krisan Alifari <akrifari@gmail.com>
2022-01-20 21:20:49 +0700

    Update gitsigns keybindings

--- .config/nvim/lua/modules/gitsigns.lua
+++ .config/nvim/lua/modules/gitsigns.lua
@@ -7,20 +7,22 @@ gitsigns.setup {
   keymaps = {
     noremap = true,
     buffer = true,
-    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<cr>'" },
-    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<cr>'" },
+    ['n ]c'] = { expr = true, "&diff ? ']c' : ':Gitsigns next_hunk<cr>'" },
+    ['n [c'] = { expr = true, "&diff ? '[c' : ':Gitsigns prev_hunk<cr>'" },
 
-    ['n gp'] = '<cmd>lua require"gitsigns".preview_hunk()<cr>',
-    ['n <leader>gs'] = '<cmd>lua require"gitsigns".stage_hunk()<cr>',
-    ['n <leader>gu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<cr>',
-    ['n <leader>gr'] = '<cmd>lua require"gitsigns".reset_hunk()<cr>',
-    ['n <leader>gR'] = '<cmd>lua require"gitsigns".reset_buffer()<cr>',
-    ['n <leader>gb'] = '<cmd>lua require"gitsigns".blame_line(true)<cr>',
-    ['n <leader>gB'] = '<cmd>Gitsigns toggle_current_line_blame<cr>',
+    ['n gp'] = ':Gitsigns preview_hunk<cr>',
+    ['n <leader>gs'] = ':Gitsigns stage_hunk<cr>',
+    ['n <leader>gS'] = ':Gitsigns stage_buffer<cr>',
+    ['n <leader>gu'] = ':Gitsigns undo_stage_hunk<cr>',
+    ['n <leader>gU'] = ':Gitsigns reset_buffer_index<cr>',
+    ['n <leader>gr'] = ':Gitsigns reset_hunk<cr>',
+    ['n <leader>gR'] = ':Gitsigns reset_buffer<cr>',
+    ['n <leader>gb'] = ':lua require"gitsigns".blame_line({full=true})<cr>',
+    ['n <leader>gB'] = ':Gitsigns toggle_current_line_blame<cr>',
 
     -- text objects
-    ['o ih'] = ':<c-u>lua require"gitsigns.actions".select_hunk()<cr>',
-    ['x ih'] = ':<c-u>lua require"gitsigns.actions".select_hunk()<cr>',
+    ['o ih'] = ':<c-u>Gitsigns select_hunk<cr>',
+    ['x ih'] = ':<c-u>Gitsigns select_hunk<cr>',
   },
   current_line_blame_opts = {
     delay = 100
