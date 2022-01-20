012fd8187210d351f4f433eecf9c608324001334
Krisan Alifari <akrifari@gmail.com>
2022-01-20 21:07:30 +0700

    Add guard for lua modules

--- .config/nvim/lua/modules/treesitter.lua
+++ .config/nvim/lua/modules/treesitter.lua
@@ -1,4 +1,9 @@
-require('nvim-treesitter.configs').setup {
+local ok, nvim_treesitter = pcall(require, 'nvim-treesitter.configs')
+if not ok then
+  return
+end
+
+nvim_treesitter.setup {
   ensure_installed = "maintained",
   highlight = {
     enable = true,
