822b99cc4f610290ad0b3414ac3a54c5f44259a7
Krisan Alifari <akrifari@gmail.com>
2022-04-18 16:05:05 +0700

    Install all treesitter parsers

--- .config/nvim/lua/modules/treesitter.lua
+++ .config/nvim/lua/modules/treesitter.lua
@@ -4,7 +4,7 @@ if not ok then
 end
 
 nvim_treesitter.setup {
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
