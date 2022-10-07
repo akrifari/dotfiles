9c24245041b24e188c40d00e51c314574fe55f16
Krisan Alifari <akrifari@gmail.com>
2022-10-07 14:55:45 +0700

    Remove plenary.nvim from gitsigns dependency

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -65,7 +65,6 @@ packer.startup(function(use)
     'tpope/vim-fugitive',
     {
       'lewis6991/gitsigns.nvim',
-      requires = 'nvim-lua/plenary.nvim',
       config = function()
         require 'modules.gitsigns'
       end,
